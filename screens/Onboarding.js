50284372a6f116cb92f7e1d4844cb87c303db51d
Aula AndroidStudio <aula.android@fatectq.local>
2023-08-21 11:19:25 -0300

    App React

---
+++ screens/Onboarding.js
@@ -0,0 +1,101 @@
+import React from "react";
+import {
+  ImageBackground,
+  Image,
+  StyleSheet,
+  StatusBar,
+  Dimensions
+} from "react-native";
+import { Block, Button, Text, theme } from "galio-framework";
+
+const { height, width } = Dimensions.get("screen");
+
+import argonTheme from "../constants/Theme";
+import Images from "../constants/Images";
+
+class Onboarding extends React.Component {
+  render() {
+    const { navigation } = this.props;
+
+    return (
+      <Block flex style={styles.container}>
+        <StatusBar hidden />
+        <Block flex center>
+        <ImageBackground
+            source={Images.Onboarding}
+            style={{ height, width, zIndex: 1 }}
+          />
+        </Block>
+        <Block center>
+          <Image source={Images.LogoOnboarding} style={styles.logo} />
+        </Block>
+        <Block flex space="between" style={styles.padded}>
+            <Block flex space="around" style={{ zIndex: 2 }}>
+              <Block style={styles.title}>
+                <Block>
+                  <Text color="black" size={24}>
+                    Hi, i'm
+                  </Text>
+                </Block>
+                <Block>
+                  <Text color="black" size={60}>
+                    Yan M.
+                  </Text>
+                </Block>
+                <Block style={styles.subTitle}>
+                  <Text color="black" size={16}>
+                    Project for 4º Semester.
+                  </Text>
+                </Block>
+              </Block>
+              <Block center>
+                <Button
+                  style={styles.button}
+                  color={argonTheme.COLORS.SECONDARY}
+                  onPress={() => navigation.navigate("App")}
+                  textStyle={{ color: argonTheme.COLORS.BLACK }}
+                >
+                  See projects
+                </Button>
+              </Block>
+          </Block>
+        </Block>
+      </Block>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: theme.COLORS.BLACK
+  },
+  padded: {
+    paddingHorizontal: theme.SIZES.BASE * 2,
+    position: "relative",
+    bottom: theme.SIZES.BASE,
+    zIndex: 2,
+  },
+  button: {
+    width: width - theme.SIZES.BASE * 4,
+    height: theme.SIZES.BASE * 3,
+    shadowRadius: 0,
+    shadowOpacity: 0
+  },
+  logo: {
+    width: 200,
+    height: 200,
+    zIndex: 2,
+    position: 'relative',
+    marginTop: '-50%',
+  },
+  title: {
+    marginTop:'-5%',
+    color: theme.COLORS.BLACK,
+  },
+  subTitle: {
+    marginTop: 20,
+    color: theme.COLORS.BLACK,
+  }
+});
+
+export default Onboarding;
